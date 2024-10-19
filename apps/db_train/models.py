7523a9a97d6dd5b07e4223e65f274f20cbf57d6f
Vadim Zhukov <apexknight@yandex.ru>
2024-10-19 09:41:06 +0300

    added model

--- apps/db_train/models.py
+++ apps/db_train/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from datetime import datetime
 
+
 # Создайте свои модели здесь
 
 class Author(models.Model):
     phone_regex = RegexValidator(
-            regex=r'^\+79\d{9}$',
-            message="Телефонный номер должен быть формата: '+79123456789'."
-        )
+        regex=r'^\+79\d{9}$',
+        message="Телефонный номер должен быть формата: '+79123456789'."
+    )
 
     username = models.SlugField(unique=True,
                                 verbose_name='Имя аккаунта',
@@ -28,11 +29,11 @@ class Author(models.Model):
                                  )
 
     middle_name = models.CharField(max_length=100,
-                                 verbose_name='Отчество',
-                                 help_text="Ограничение - не более 100 символов",
-                                 null=True,
-                                 blank=True,
-                                 )
+                                   verbose_name='Отчество',
+                                   help_text="Ограничение - не более 100 символов",
+                                   null=True,
+                                   blank=True,
+                                   )
 
     gender = models.CharField(max_length=1,
                               choices=[('м', 'мужской'), ('ж', 'женский')],
@@ -98,7 +99,7 @@ class Author(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        if self.date_birth:# Если известен день рождения
+        if self.date_birth:  # Если известен день рождения
             today = datetime.today()  # Определяем текущие параметры даты
             # Определяем добавку, был ли уже день рождения в этом году? Если не был, то 1, если был, то 0
             additional_year = (today.month, today.day) < (self.date_birth.month, self.date_birth.day)
@@ -110,6 +111,34 @@ class Author(models.Model):
         if self.first_name and self.middle_name:
             initials = f"{self.first_name.upper()[0]}.{self.middle_name.upper()[0]}."
         return f"{self.username} - {self.last_name} {initials}"
+
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
+
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField('Author', on_delete=models.CASCADE)
+    stage = models.IntegerField(default=0,
+                                blank=True,
+                                verbose_name="Стаж",
+                                help_text="Стаж в годах")
+
+    def __str__(self):
+        return f"Автор: {self.author.username}; Стаж: {self.stage} лет"
+
+
+class Entry(models.Model):
+    text = models.TextField(verbose_name="Текст статьи", )
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name='entries')
+    tags = models.ManyToManyField("Tag", related_name='entries')
+
+    def __str__(self):
+        return f"Автор: {self.author}; Тег: {self.tags}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Название", )
+
+    def __str__(self):
+        return f'{self.name}'
